d6875009a151b6b6e2ebf158074f4d6a7fe971ae
Eamon McHugh <eamonmchugh2@gmail.com>
2022-07-14 15:37:14 -0400

    beginning of portfolio. created react app. added basic components and sass.

---
+++ react-portfolio-2022/src/index.js
@@ -0,0 +1,19 @@
+// Render dependencies
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+// Import app
+import App from './App.jsx';
+
+// Import styles
+import './sass/style.scss';
+
+// Render to dom
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
+	<StrictMode>
+		<App />
+	</StrictMode>
+);
